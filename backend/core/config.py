6ed85a9f7524856d3a96de01b71fe3deb32ddd22
joelDigbeu <ephraimdigbeu@gmail.com>
2021-07-30 12:26:50 +0000

    create mysql database connection

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,21 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 class Settings:
     PROJECT_NAME: str = "Jobboard"
     PROJECT_VERSION: str = "0.1.0"
+
+    DB_CONNECTION: str = os.getenv('DB_CONNECTION')
+    DB_NAME: str = os.getenv('DB_NAME')
+    DB_HOST: str = os.getenv('DB_HOST')
+    DB_PORT: str = os.getenv('DB_PORT')
+    DB_USER: str = os.getenv('DB_USER')
+    DB_PASSWORD: str = os.getenv('DB_PASSWORD')
+
+    DB_URL= f"${DB_CONNECTION}://${DB_USER}:${DB_PASSWORD}@{DB_HOST}:${DB_PORT}/${DB_NAME}"
 
 
 settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from typing import Optional
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
 
 app = FastAPI(title = settings.PROJECT_NAME, version = settings.PROJECT_VERSION)
 
