8e7f211eddeda6deb7756c039b1e1381c0f8d007
joelDigbeu <ephraimdigbeu@gmail.com>
2021-08-02 11:24:38 +0000

    add dependency injection to session.py

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,3 +1,4 @@
+from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -7,3 +8,11 @@ SQLALCHEMY_DATABASE_URL = settings.DB_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = sessionLocal()
+        yield db
+    finally:
+        db.close()
