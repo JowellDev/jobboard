5afb04f9886629b47ccf4366ea8bc4c418e970c1
JowellDev <ephraimdigbeu@gmail.com>
2021-08-03 10:04:06 +0000

    refactoring create user endpoint

---
+++ backend/api/base.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from api.v1 import route_users
+
+
+api_router = APIRouter()
+
+api_router.include_router(route_users.router, prefix="/user", tags=["users"])
--- backend/main.py
+++ backend/main.py
@@ -3,14 +3,19 @@ from typing import Optional
 from core.config import settings
 from db.session import engine
 from db.base import Base
+from api.base import api_router
 
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
+def include_router(app):
+    app.include_router(api_router)
+
 def run_app():
     app = FastAPI(title = settings.PROJECT_NAME, version = settings.PROJECT_VERSION)
     create_tables()
+    include_router(app)
     return app
 
 app = run_app()
