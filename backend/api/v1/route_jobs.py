14ac20ce1a0c6e0d702bb14bc4c0ebfdff5e999c
JowellDev <ephraimdigbeu@gmail.com>
2021-08-31 12:59:57 +0000

    update job post

--- backend/api/v1/route_jobs.py
+++ backend/api/v1/route_jobs.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from db.session import get_db
 from schemas.jobs import ShowJob, JobSchema
@@ -7,6 +7,6 @@ from repository.jobs import create_new_job
 router = APIRouter()
 
 @router.post('/', response_model=ShowJob)
-def create_job(job: JobSchema, db: Session = Depends(get_db)):
-    job = create_new_job(job, db)
+def create_job(job: JobSchema, owner_id: int, db: Session = Depends(get_db)):
+    job = create_new_job(job, db, owner_id)
     return job
--- backend/repository/jobs.py
+++ backend/repository/jobs.py
@@ -3,16 +3,17 @@ from schemas.jobs import JobSchema
 from models.jobs import Job
 from datetime import datetime
 
-def create_new_job(job: JobSchema, db: Session):
+def create_new_job(job: JobSchema, db: Session, owner_id: int):
 
     new_job = Job(
+        #I can just do : **job.dict() for parse job data in my Job Model
         title = job.title,
         company = job.company,
         company_url = job.company_url,
         location = job.location,
         description = job.description,
         date_posted = datetime.now().date(),
-        #owner_id = 1
+        owner_id = owner_id
     )
 
     db.add(new_job)
