e765bb20695fdcfe7ae04beba697b78ca0deb545
JowellDev <ephraimdigbeu@gmail.com>
2021-09-03 17:44:21 +0000

    user authentification refactoring

--- backend/api/v1/route_auth.py
+++ backend/api/v1/route_auth.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
 
 from db.session import get_db
-from models.users import User
-from schemas.users import ShowUser
-from repository.login import get_user, authenticate_user, create_access_token
+from repository.login import authenticate_user, create_access_token
+from schemas.token import Token
+
 
 router = APIRouter()
 
-@router.post('/token')
-def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+@router.post('/token', response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(
--- backend/repository/login.py
+++ backend/repository/login.py
@@ -1,13 +1,11 @@
-from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from models.users import User
-from core.security import Hasher
-from jose import JWTError, jwt
+from jose import jwt
 from typing import Optional
 from datetime import datetime, timedelta
 from core.config import settings
+from models.users import User
+from core.security import Hasher
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_user(username: str, db: Session)-> User:
     user = db.query(User).filter(User.email == username).first()
@@ -24,11 +22,11 @@ def authenticate_user(username: str, password: str, db: Session):
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-        to_encode = data.copy()
-        if expires_delta:
-            expire = datetime.utcnow() + expires_delta
-        else:
-            expire = datetime.utcnow() + timedelta(minutes = settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-        to_encode.update({"exp": expire})
-        encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm = settings.JWT_ALGORITHM)
-        return encoded_jwt
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes = settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm = settings.JWT_ALGORITHM)
+    return encoded_jwt
